edf21b537e9f640cf16105bd399c318c9d1b2826
Benjamin Staneck <staneck@gmail.com>
2013-11-23 23:36:13 +0100

    bla bla spaces

--- custom.barcolours.lua
+++ custom.barcolours.lua
@@ -8,11 +8,11 @@ local kn = LibStub('AceAddon-3.0'):GetAddon('KuiNameplates')
 local mod = kn:NewModule('CustomInjector', 'AceEvent-3.0')
 
 local units = {
-    ['Stanzilla'] = { 1, 0, 0 },
-    ["Kor'kron Assassin"] = { 1, 0.96, 0.41 },
+	['Stanzilla'] = { 1, 0, 0 },
+	["Kor'kron Assassin"] = { 1, 0.96, 0.41 },
 	["Kor'kron Arcweaver"] = { 0.41, 0.80, 0.94 },
 	["Kor'kron Warshaman"] = { 0, 0.44, 0.87 },
-    ['Farseer Wolf Rider'] = { 1, 0, 1.25 }
+	['Farseer Wolf Rider'] = { 1, 0, 1.25 }
 }
 
 function mod:PostShow(msg, frame)
